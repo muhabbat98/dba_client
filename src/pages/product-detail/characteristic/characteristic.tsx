807b9c4df8bd8eafd121c52026ec63521f00f731
Jamoliddinoff <arrow97ivat@gmail.com>
2021-06-02 14:41:25 +0300

    ..

--- src/pages/product-detail/characteristic/characteristic.tsx
+++ src/pages/product-detail/characteristic/characteristic.tsx
@@ -1,7 +1,7 @@
 import React,{useState} from 'react'
 import ProductTitle from '../../../components/products-title';
 import Input from '../../../components/input';
-import { useForm } from "react-hook-form";
+
 import {
     WrapperContainer,
     CharacterContainer,
@@ -35,12 +35,7 @@ const network = [
 ]
 const Characteristic = () => {
     const [characters,setCharacters] = useState<boolean>(false)
-    const [inp,setInp] = useState<any>('');
-    
-    const {control} = useForm();
-    const handleChange = (e:any) =>{
-        console.log('uu--->',e.target.value)
-    }
+   
     return (
         <div>
             <ProductTitle title="Характеристика"/>
@@ -97,7 +92,7 @@ const Characteristic = () => {
             </WrapperContainer>
             </>}
             <HandeleOpen onClick={()=>setCharacters(open=>!open)}>{characters?"Свернуть характеристики":"Развернуть характеристики"}</HandeleOpen>
-            <Input control={control} label="Number" inputType="brithDay" onChange={(e)=>handleChange(e)} defaultValue={inp}/> 
+           
         </div>
     )
 }
