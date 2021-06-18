c618d28d46d1a255b170540dd6b611fe96160405
Jamoliddinoff <arrow97ivat@gmail.com>
2021-06-18 10:48:14 +0300

    cutom inputMask

--- src/utils/input-letter.ts
+++ src/utils/input-letter.ts
@@ -4,6 +4,38 @@ const inputLetter = (e:any) =>{
 export default inputLetter;
 
 export const phoneMask = (e:any) => {
-    return e.target.value.replace(/[^0-9]/g, "")
-    // return e.target.value.replace(/\D/g, "")
+    return e.target.value.replace(/\D/g, '')
+    .replace(/^(\d)/, '($1')
+    .replace(/^(\(\d{2})(\d)/, '$1)$2')
+    .replace(/(\d{3})(\d{1,5})/, '$1-$2')
+    .replace(/(-\d{2})(\d{1,5})/, '$1-$2')
+    .replace(/(-\d{2})\d+?$/, '$1');
 }
+export const passport = (e:any) => {
+    return e.target.value.replace(/\D/g, '')
+    
+    .replace(/(\D{2})(\d{1,5})/, '$1-$2')
+    .replace(/(-\d{7})\d+?$/, '$1');
+}
+export const cardNumber = (e:any) => {
+    return e.target.value.replace(/\D/g, '')
+    .replace(/(\d{4})(\d{1,3})/, '$1-$2')
+    .replace(/(\d{4})(\d{1,3})/, '$1-$2')
+    .replace(/(\d{4})(\d{1,3})/, '$1-$2')
+    .replace(/(-\d{4})\d+?$/, '$1');
+}
+export const cardDate = (e:any) => {
+    return e.target.value.replace(/\D/g, '')
+    .replace(/(\d{2})(\d{1,3})/, '$1/$2')
+    .replace(/(\/\d{2})\d+?$/, '$1');
+}
+export const inn = (e:any) => {
+    return e.target.value.replace(/\D/g, '')
+    .replace(/(\d{3})(\d{1,3})/, '$1-$2')
+    .replace(/(-\d{6})\d+?$/, '$1');
+}
+// replace(/\D/g, '')
+//     .replace(/^(\d)/, '($1')
+//     .replace(/^(\(\d{2})(\d)/, '$1) $2')
+//     .replace(/(\d{4})(\d{1,5})/, '$1-$2')
+//     .replace(/(-\d{5})\d+?$/, '$1');
