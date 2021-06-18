b8b88ee1766b9fb4092e28970f8ec7bd91eab88a
Jamoliddinoff <arrow97ivat@gmail.com>
2021-06-18 10:51:07 +0300

    .

--- src/utils/input-letter.ts
+++ src/utils/input-letter.ts
@@ -14,7 +14,7 @@ export const phoneMask = (e:any) => {
 export const passport = (e:any) => {
     return e.target.value.replace(/\D/g, '')
     
-    .replace(/(\D{2})(\d{1,5})/, '$1-$2')
+    .replace(/(\d{2})(\d{1,5})/, '$1-$2')
     .replace(/(-\d{7})\d+?$/, '$1');
 }
 export const cardNumber = (e:any) => {
