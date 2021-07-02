a8555521adc1e9c6c5d39a1af5c28185a707b269
Mirodil Fayzullayev <odilfayz22@gmail.com>
2021-07-02 10:27:19 +0500

    .

---
+++ src/admin2/components/admin-protected-route/admin-protected-route.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Route, Redirect, RouteProps } from 'react-router-dom';
+import { useSelector } from '../../../hooks';
+
+const checkUSer = (user: any) => {
+  if (
+    (user.accessToken &&
+      user.tokenType &&
+      user.id &&
+      user.roles === 'ROLE_MODERATOR') ||
+    user.roles === 'ROLE_ADMIN'
+  ) {
+    return true;
+  }
+
+  return false;
+};
+
+const AdminProtectedRoute: React.FC<RouteProps> = ({ children, ...rest }) => {
+  // const user = useSelector((state) => checkUSer(state.user));
+  const user = true;
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        user ? (
+          children
+        ) : (
+          <Redirect to={{ pathname: '/', state: { from: location } }} />
+        )
+      }
+    />
+  );
+};
+
+export default AdminProtectedRoute;
