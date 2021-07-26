875e0856e603a4a898afc6735cfd074141e42283
muhabbat98 <bmpmuhabbatpulatova98@gmail.com>
2021-07-26 17:40:34 +0500

    initial checkout

--- src/pages/check-out/check-out.tsx
+++ src/pages/check-out/check-out.tsx
@@ -1,6 +1,55 @@
 import React from 'react';
+import { useSelector } from '../../hooks';
+import Container from '../../components/grid/container';
+import {
+  SmallerContainer,
+  HomeLink,
+  CheckOutLink,
+  CartLink,
+  CheckOutHeader,
+  CheckOutBox,
+  CartContain,
+  CartHeader,
+  CountItems,
+  CartItemText,
+  CartContainItem,
+  CartItemMedia,
+  CartItemImg,
+} from './style';
 
 const CheckOut = () => {
-  return <div>CheckOut</div>;
+  const { cart } = useSelector((state) => state);
+
+  console.log('cart => ', cart);
+
+  return (
+    <Container>
+      <SmallerContainer>
+        <HomeLink to="/">Главная</HomeLink>
+        <CartLink to="/cart">Корзина</CartLink>
+        <CheckOutLink to="/check-out">Оформление заказа</CheckOutLink>
+        <CheckOutHeader>Оформление заказа</CheckOutHeader>
+        <CheckOutBox>
+          <CartHeader>
+            Корзина (<CountItems>12</CountItems>)
+          </CartHeader>
+          <CartContain>
+            {cart.checkedAll.map((element, index) => (
+              <CartContainItem key={index}>
+                <CartItemMedia>
+                  <CartItemImg src={element.images[0]} />
+                </CartItemMedia>
+                <CartItemText>
+                  {' '}
+                  Смартфон Xiaomi Redmi 9 4/64GB (NFC)
+                </CartItemText>
+              </CartContainItem>
+            ))}
+          </CartContain>
+        </CheckOutBox>
+        <CheckOutBox></CheckOutBox>
+      </SmallerContainer>
+    </Container>
+  );
 };
 export default CheckOut;
--- src/pages/check-out/style.ts
+++ src/pages/check-out/style.ts
@@ -1,1 +1,73 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+export const CheckOutLink = styled(Link)``;
+export const CartLink = styled(Link)``;
+export const HomeLink = styled(Link)``;
+
+export const SmallerContainer = styled.div`
+  max-width: 588px;
+  margin: 0 auto;
+  a {
+    text-decoration: none;
+    font-size: 16px;
+    color: ${({ theme }) => theme.grey1};
+    font-weight: 400;
+    line-height: 20px;
+  }
+  a::after {
+    content: '';
+  }
+  a:not(:last-child)::after {
+    width: 1px;
+    height: 16px;
+    display: inline-block;
+    transform: skewX(-15deg);
+    margin: 0px 15px;
+    background-color: ${({ theme }) => theme.grey1};
+  }
+`;
+export const CheckOutHeader = styled.h3`
+  font-size: 24px;
+  letter-spacing: 0.5px;
+  line-height: 1.5rem;
+`;
+export const CheckOutBox = styled.div`
+  padding: 2.7rem;
+  border-radius: 16px;
+  border: 1px solid ${({ theme }) => theme.light_grey};
+`;
+export const CartHeader = styled.h3`
+  font-size: 24px;
+  line-height: 1.5rem;
+`;
+export const CountItems = styled.span``;
+
+export const CartContain = styled.div``;
+export const CartContainItem = styled.div`
+  margin: 24px 0px;
+  display: flex;
+`;
+export const CartItemMedia = styled.div`
+  padding: 8px;
+  border: 1px solid ${({ theme }) => theme.grey3};
+  border-radius: 8px;
+`;
+export const CartItemImg = styled.img`
+  width: 84px;
+  height: 84px;
+  object-fit: cover;
+`;
+export const CartItemText = styled.div`
+  padding: 0px 24px;
+  font-size: 16px;
+  line-height: 22px;
+  &::after {
+    content: '1 шт.';
+    display: block;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 22px;
+    padding: 12px 0px;
+    color: ${({ theme }) => theme.grey1};
+  }
+`;
