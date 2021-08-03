475cb946ba3cae1b38fb8c2635000b5104548e33
Jamoliddinoff <arrow97ivat@gmail.com>
2021-08-03 10:44:00 +0500

    buyer and seller settings sideBar hidden

--- src/pages/settings-for-buyer/settings-for-buyer.tsx
+++ src/pages/settings-for-buyer/settings-for-buyer.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import BuyerMenu from "./buyer-menu";
 import BuyerContent from "./buyer-content";
 import Title from '../../components/products-title';
+import {useWindowSize} from '../../hooks/useWindowSize'
 import {SettingsForBuyerContainer, MenuAndContent} from './style';
 import Container from "../../components/grid/container";
 
 const SettingsForBuyer = () => {
+    const [width,height] = useWindowSize();
   return (
       <Container>
           <SettingsForBuyerContainer>
               <Title>Настройки </Title>
               <MenuAndContent>
-                  <BuyerMenu />
-                  <BuyerContent />
+                {width>768 &&
+                    <BuyerMenu />
+                }
+                <BuyerContent />
               </MenuAndContent>
           </SettingsForBuyerContainer>
       </Container>
