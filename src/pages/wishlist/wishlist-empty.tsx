9f79e12511fa7e66fd9b8e8b4ad1f3a3cc41960d
IamDilshod <46994171+IamDilshod@users.noreply.github.com>
2021-08-10 09:24:13 +0500

    aaa

--- src/pages/wishlist/wishlist-empty.tsx
+++ src/pages/wishlist/wishlist-empty.tsx
@@ -1,36 +1,55 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { WishlistEmptyContainer, WishlistEmptyImg, WishlistEmptyTextBox, WishlistEmptyTitle, WishlistEmptyText } from './style';
-import { ReactComponent as WishlistEmptyIcon } from '../../assets/icons/love.svg'
+import {
+  WishlistEmptyContainer,
+  WishlistEmptyImg,
+  WishlistEmptyTextBox,
+  WishlistEmptyTitle,
+  WishlistEmptyText,
+} from './style';
+import { ReactComponent as WishlistEmptyIcon } from '../../assets/icons/love.svg';
 import Col from '../../components/grid/col';
 import Button from '../../components/button';
 
 const WishlistEmpty = () => {
-   const {push} = useHistory();
+  const { push } = useHistory();
 
-   const goHome = () =>{
-      push("/");
-   }
+  const goHome = () => {
+    push('/');
+  };
 
-   return (
-      <Col xl={12}>
-         <WishlistEmptyContainer>
-            <WishlistEmptyImg>
-               <WishlistEmptyIcon />
-            </WishlistEmptyImg>
-            <WishlistEmptyTextBox>
-               <WishlistEmptyTitle>Ещё не готовы к покупке?</WishlistEmptyTitle>
-               <WishlistEmptyText>Жмите ♡ на странице товара и добавляйте сюда то,
-                  что нравится. И если цена на эти товары упадёт,
-                  мы вам напишем.</WishlistEmptyText>
-            </WishlistEmptyTextBox>
-            <div style={{ display: 'flex', alignItems: "center", flexDirection: 'column' }}>
-               <Button onClick={goHome} style={{marginBottom: "24px"}}>Перейти на Главную</Button>
-               {/* <Button btnType="disabled" style={{ cursor: "pointer" }}>Войти в свой аккаунт</Button> */}
-            </div>
-         </WishlistEmptyContainer>
-      </Col>
-   )
-}
+  return (
+    <Col xl={12}>
+      <WishlistEmptyContainer>
+        <WishlistEmptyImg>
+          <WishlistEmptyIcon />
+        </WishlistEmptyImg>
+        <WishlistEmptyTextBox>
+          <WishlistEmptyTitle>Ещё не готовы к покупке?</WishlistEmptyTitle>
+          <WishlistEmptyText>
+            Жмите ♡ на странице товара и добавляйте сюда то, что нравится. И
+            если цена на эти товары упадёт, мы вам напишем.
+          </WishlistEmptyText>
+        </WishlistEmptyTextBox>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            flexDirection: 'column',
+          }}
+        >
+          <Button
+            size="large"
+            onClick={goHome}
+            style={{ marginBottom: '24px' }}
+          >
+            Перейти на Главную
+          </Button>
+          {/* <Button btnType="disabled" style={{ cursor: "pointer" }}>Войти в свой аккаунт</Button> */}
+        </div>
+      </WishlistEmptyContainer>
+    </Col>
+  );
+};
 
 export default WishlistEmpty;
